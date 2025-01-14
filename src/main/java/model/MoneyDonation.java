43a096c801b307292e49edd2106530a1f0602a73
yahia zaki <yahia.az.yz@gmail.com>
2025-01-15 01:43:25 +0200

    Template updated

--- src/main/java/model/MoneyDonation.java
+++ src/main/java/model/MoneyDonation.java
@@ -5,15 +5,39 @@ import java.util.Date;
 public class MoneyDonation extends Donation{
 
     private String currencyType;
+    private double amount;
 
-    public MoneyDonation(double amount, Date date, int personId, String currencyType) {
-        super(amount, date, personId);
+    public MoneyDonation(Date date, int personId, String currencyType,double amount) {
+        super(date, personId);
         this.currencyType = currencyType;
+        this.amount = amount;
+    }
+
+    public String getCurrencyType() {
+        return currencyType;
+    }
+
+    public void setCurrencyType(String currencyType) {
+        this.currencyType = currencyType;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
     }
 
     @Override
     boolean validateDonation(Donation donation) {
-        return donation.getAmount() > 1;
+        if (!(donation instanceof MoneyDonation)) {
+            return false;
+        }
+
+        MoneyDonation moneyDonation = (MoneyDonation) donation;
+
+        return moneyDonation.getAmount() > 1;
     }
 
     @Override
