b8e071251cc8ba256c9c169cb9ca77981b4f7206
boudyK <abdalrahmank2000@gmail.com>
2025-01-16 21:03:41 +0200

    quick fix

--- src/main/java/com/example/demo2/MainApp.java
+++ src/main/java/com/example/demo2/MainApp.java
@@ -13,7 +13,7 @@ import java.util.Date;
 public class MainApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("LoginController.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("LoginPage.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Login Page");
         stage.setScene(scene);
