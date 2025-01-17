91267b7d3654998b4244b9e8ccd6b198038e09ea
yahia zaki <yahia.az.yz@gmail.com>
2025-01-17 21:20:18 +0200

    CampaignTotalAmountController updated

--- src/main/java/com/example/demo2/CampaignTotalAmountController.java
+++ src/main/java/com/example/demo2/CampaignTotalAmountController.java
@@ -1,18 +1,47 @@
 package com.example.demo2;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
 import javafx.stage.Stage;
+import model.Campaign;
 import model.DesignPatterns.observer.IObserver;
 import model.DesignPatterns.strategy.UserLoginContext;
 import model.Volunteer;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class CampaignTotalAmountController implements IObserver {
+    private ArrayList<Campaign> retrievedCampaigns;
+    @FXML
+    private ComboBox<String> campaignToView;
+
+    @FXML
+    public void initialize() {
+        retrievedCampaigns = Campaign.retrieveAllCampaigns();
+        ObservableList<String> observableCampaigns = FXCollections.observableArrayList();
+        for (Campaign campaign : retrievedCampaigns) {
+            observableCampaigns.add(campaign.getTitle());
+        }
+        campaignToView.setItems(observableCampaigns);
+    }
+
+
+    @FXML
+    private void viewCampaign(javafx.event.ActionEvent event) throws IOException{
+
+        int selectedCampaignIndex = campaignToView.getSelectionModel().getSelectedIndex();
+
+
+    }
+
+
     @FXML
     private void gotoMainPage(javafx.event.ActionEvent event) throws IOException {
         String pageTitle;
